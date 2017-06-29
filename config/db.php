6bc21e199cd9a8c4c5a2b8eb6b96908e24430ca8
rooliva <rooliva@alumnos.ubiobio.cl>
2017-06-29 02:33:50 -0400

    Update DB

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=localhost;dbname=dsi2017',
+    'username' => 'postgres',
+    'password' => 'udeconce',
     'charset' => 'utf8',
 ];
